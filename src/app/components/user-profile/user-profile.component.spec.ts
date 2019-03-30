2f12a12bd777db0125af423910cddda8f4760050
gopigoppu <gopi.learner93@gmail.com>
2019-03-30 12:17:42 +0530

    user profile unit test add

--- src/app/components/user-profile/user-profile.component.spec.ts
+++ src/app/components/user-profile/user-profile.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserProfileComponent } from './user-profile.component';
+import { ActivatedRoute } from '@angular/router';
+import { observerSuccessStub } from '../../../utt-helpers/async-helpers';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
 
+  const data = {
+    items: {
+      name: 'Gopi',
+      avatar_url: 'http://www.beoxi.com',
+      bio: 'Angular Developer',
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
+      declarations: [UserProfileComponent],
+      providers: [{ provide: ActivatedRoute, useValue: { snapshot: { data } } }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +30,15 @@ describe('UserProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create compoent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('component should render with resolved user deatils', () => {
+    const hostElement = fixture.nativeElement;
+    const userNameEl: HTMLInputElement = hostElement.querySelector('h4.card-title');
+    expect(userNameEl.textContent).toContain('Gopi');
+    const userBioEl: HTMLInputElement = hostElement.querySelector('p.card-text');
+    expect(userBioEl.textContent).toContain('Angular Developer');
   });
 });
