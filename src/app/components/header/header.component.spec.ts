974d769b8f7ac9af14b30050094ad1a7d7ced105
gopigoppu <gopi.learner93@gmail.com>
2019-03-30 12:08:01 +0530

    header component unit test scripts add

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { click } from '../../../utt-helpers/index';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { RegisterUserComponent } from '../../modal/register-user/register-user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,8 +16,15 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      imports: [ReactiveFormsModule],
+      declarations: [HeaderComponent, RegisterUserComponent],
+      providers: [NgbModal]
     })
+      .overrideModule(BrowserDynamicTestingModule, {
+        set: {
+          entryComponents: [RegisterUserComponent],
+        }
+      })
       .compileComponents();
   }));
 
@@ -19,7 +34,39 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render initial header contents - using querySelector', () => {
+    console.log(fixture);
+    const headerDe: DebugElement = fixture.debugElement;
+    const headerEl: HTMLElement = headerDe.nativeElement;
+    const navHeader = headerEl.querySelector('div');
+    expect(navHeader.textContent).toContain('Angular Unit Test - Beoxi');
+  });
+
+  it('should render initial header contents - using Debugelement BY CSS helper', () => {
+    console.log(fixture);
+    const bannerDe: DebugElement = fixture.debugElement;
+    const navHeaderDe = bannerDe.query(By.css('div'));
+    expect(navHeaderDe.nativeElement.textContent).toContain('Angular Unit Test - Beoxi');
+    const buttonDe = bannerDe.query(By.css('.pointer-cursor'));
+    expect(buttonDe.nativeElement.textContent).toContain('Register');
+  });
+
+  it('register click should open model popup with title Register New User', () => {
+    console.log(fixture);
+    const modalRes = {
+      componentInstance: {
+        title: 'hello'
+      }
+    };
+    spyOn((component as any).modalService, 'open').and.returnValue(modalRes);
+    const bannerDe: DebugElement = fixture.debugElement;
+    const buttonDe = bannerDe.query(By.css('.pointer-cursor'));
+    click(buttonDe);
+    expect((component as any).modalService.open).toHaveBeenCalled();
+    expect(buttonDe.nativeElement.textContent).toContain('Register');
   });
 });
